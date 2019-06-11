3b7d9e170bc57f8eda41d1172ef54728435699a9
weiya <jiaweiya@didiglobal.com>
2019-06-11 18:52:46 +0800

    :sparkles: 修正命令行显示 qr code

--- GFWeather.py
+++ GFWeather.py
@@ -106,9 +106,9 @@ class GFWeather:
         for _ in range(5):
             # 命令行显示登录二维码。
             if os.environ.get('MODE') == 'server':
-                itchat.auto_login(enableCmdQR=2, hotReload=True)
+                itchat.auto_login(enableCmdQR=True)
             else:
-                itchat.auto_login(hotReload=True)
+                itchat.auto_login(enableCmdQR=True)
             if _online():
                 print('登录成功')
                 return True
